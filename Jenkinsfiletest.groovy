39fb40c271203a20c6baa44884e5e141d1077ca4
ismailyashar <68939431+ismailyashar@users.noreply.github.com>
2020-09-24 22:27:41 -0500

    Create Jenkinsfiletest.groovy

---
+++ Jenkinsfiletest.groovy
@@ -0,0 +1,30 @@
+properties([
+    parameters([
+        string(defaultValue: '', description: 'Input node IP', name: 'SSHNODE', trim: true)
+        ])
+    ])
+
+node {
+    withCredentials([sshUserPrivateKey(credentialsId: 'Jenkins-master', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
+    
+        stage("Install git") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install git -y"
+        }       
+        stage("Clone Repo") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } git clone https://github.com/anfederico/Flaskex "
+        }
+        stage("Install pip3") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install python3-pip -y"
+            
+        }
+        stage("Built Application") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } pip3 install -r requirements.txt"
+            
+         stage("Built Application") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } python3 app.py"
+            
+        }
+        }
+        
+    }
+}
